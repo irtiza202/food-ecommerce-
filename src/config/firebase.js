e6103cbdbd1fbeabef907290363adecf70000b15
irtiza202 <irtizamouzum@gmail.com>
2024-11-03 18:50:42 +0500

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,14 +4,7 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCEPeRLt3VydcsWOrREcvARuHV6iFQCulA",
-  authDomain: "e-commerce-1212.firebaseapp.com",
-  projectId: "e-commerce-1212",
-  storageBucket: "e-commerce-1212.firebasestorage.app",
-  messagingSenderId: "53549594004",
-  appId: "1:53549594004:web:fc1ec0c82e061e863fcb81"
-};
+
 
 
 const app = initializeApp(firebaseConfig);
